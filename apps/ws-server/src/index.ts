8e5be4450a32067f3a28f0f29955ec9f46e497a1
Vinit Raj Singh <vinitrajsin@gmail.com>
2025-09-09 01:29:54 +0530

    Broadcasting message done

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,7 +1,19 @@
-import {WebSocketServer} from "ws";
+import {WebSocketServer, WebSocket} from "ws";
 
 const wss = new WebSocketServer({port: 3002});
 
+let allSockets: WebSocket[] = [];
+
 wss.on("connection", (socket)=> {
+    allSockets.push(socket);
     console.log("Client connected");
+
+    socket.on("message", (message) => {
+        console.log("Received:", message.toString());
+        allSockets.forEach(s => s.send(message.toString() + " from server"));
+    })
+    socket.on("disconnect", () => {
+    allSockets = allSockets.filter(s => s != socket);
+    });
 })
+
